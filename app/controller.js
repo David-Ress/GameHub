ecce6f6af47b3ead397827f3b1980675469baae3
David-Ress <david.ressicaud@hotmail.fr>
2022-10-31 11:01:58 +0100

    Etape 5: dynamisation des pages /game/:nomDuJeu , et création de la page 404

--- app/controller.js
+++ app/controller.js
@@ -3,19 +3,40 @@ const games = require('../games.json');
 const controller = {
   getHomepage(req,res){
     res.locals.games = games;
-    res.locals.css = '';
-    res.render('index'); // nom du fichier .ejs lié
+    res.locals.css = null;
+    res.render('index');
   },
-  getFourchette(req,res){
-    res.locals.games = games;
-    res.locals.css = '';
-    res.render('fourchette');
-  },
-  getDiceRoller(req,res){
-    res.locals.games = games;
-    res.locals.css = 'diceRoller';
-    res.render('diceRoller');
+  // getFourchette(req,res){
+  //   res.locals.games = games;
+  //   res.locals.css = '';
+  //   res.render('fourchette');
+  // },
+  // getDiceRoller(req,res){
+  //   res.locals.games = games;
+  //   res.locals.css = 'diceRoller';
+  //   res.render('diceRoller');
+  // },
+  getGame(req,res){
+    const nomDuJeu = req.params.nomDuJeu;
+
+    const gamesData = games.find( game => game.name === nomDuJeu);
+
+    console.log(gamesData);
+    
+    if(gamesData){
+      res.locals.games = games;
+      res.locals.css = gamesData.cssFile;
+  
+      res.render(nomDuJeu);
+    } else {
+      res.status(404).send('Sorry cant find that!');
+    }
+    
+
+  
   },
 };
+
+ 
 
 module.exports = controller;
--- app/router.js
+++ app/router.js
@@ -4,7 +4,8 @@ const router = express.Router();
 const controller = require('./controller.js');
 
 router.get('/', controller.getHomepage);
-router.get('/game/fourchette', controller.getFourchette);
-router.get('/game/diceRoller', controller.getDiceRoller);
+// router.get('/game/fourchette', controller.getFourchette);
+// router.get('/game/diceRoller', controller.getDiceRoller);
+router.get('/game/:nomDuJeu', controller.getGame);
 
 module.exports = router;
