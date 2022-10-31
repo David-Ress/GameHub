04e725f593862d2c2a4a9e75f206bc627da75bc5
David-Ress <david.ressicaud@hotmail.fr>
2022-10-31 10:05:48 +0100

    Etape 3: création des routes pour les jeux, factorisation pour qu'un css spécifique soit inclus pour diceRoller

--- app/controller.js
+++ app/controller.js
@@ -1,7 +1,16 @@
 const controller = {
   getHomepage(req,res){
     // res.locals.title = '';
+    res.locals.css = '';
     res.render('index'); // nom du fichier .ejs lié
+  },
+  getFourchette(req,res){
+    res.locals.css = '';
+    res.render('fourchette');
+  },
+  getDiceRoller(req,res){
+    res.locals.css = 'diceRoller';
+    res.render('diceRoller');
   },
 };
 
--- app/router.js
+++ app/router.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const controller = require('./controller.js');
 
 router.get('/', controller.getHomepage);
+router.get('/game/fourchette', controller.getFourchette);
+router.get('/game/diceRoller', controller.getDiceRoller);
 
 module.exports = router;
