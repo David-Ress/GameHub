b5511aac766db7fcac7589b7700976a9aa125dbe
David-Ress <david.ressicaud@hotmail.fr>
2022-10-31 10:23:25 +0100

    dynamisation pour la génération des liens nav

--- app/controller.js
+++ app/controller.js
@@ -1,14 +1,18 @@
+const games = require('../games.json');
+
 const controller = {
   getHomepage(req,res){
-    // res.locals.title = '';
+    res.locals.games = games;
     res.locals.css = '';
     res.render('index'); // nom du fichier .ejs lié
   },
   getFourchette(req,res){
+    res.locals.games = games;
     res.locals.css = '';
     res.render('fourchette');
   },
   getDiceRoller(req,res){
+    res.locals.games = games;
     res.locals.css = 'diceRoller';
     res.render('diceRoller');
   },
